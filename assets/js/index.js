5a86043324e94b5f2ba7d15dc851e67bb41a355e
SSIM <ssim@SSIMui-Macmini.local>
2025-06-18 18:53:42 +0900

    good

--- assets/js/index.js
+++ assets/js/index.js
@@ -45,29 +45,6 @@ document.querySelector("#GoToTop").addEventListener("click", function () {
 
 
 
-const toggleBtn = document.querySelector('.autoplay--btn');
-const playIcon = toggleBtn.querySelector('.play_ic--block');
-const pauseIcon = toggleBtn.querySelector('.pause_ic--block');
-let isPlaying = true;
-
-playIcon.style.display = 'none';
-pauseIcon.style.display = 'block';
-
-toggleBtn.addEventListener('click', function () {
-    if (isPlaying) {
-        mainswiper.autoplay.stop();
-        toggleBtn.classList.remove('playing');
-        playIcon.style.display = 'block';
-        pauseIcon.style.display = 'none';
-    } else {
-        mainswiper.autoplay.start();
-        toggleBtn.classList.add('playing');
-        playIcon.style.display = 'none';
-        pauseIcon.style.display = 'block';
-    }
-    isPlaying = !isPlaying;
-});
-
 
 
 
